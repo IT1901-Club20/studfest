6500d347befe1abf2ffda1a9b0523ca1e33d054e
Erik Dymbe <erik.dymbe@gmail.com>
2017-10-03 11:31:06 +0200

    create hello world for band/

--- studfest/settings.py
+++ studfest/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['studentfestival.club', 'localhost', '*']
 # Application definition
 
 INSTALLED_APPS = [
+    'band.apps.BandConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
