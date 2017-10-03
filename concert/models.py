982053ad2040032a8e802078f1db6084f514c876
jonasbgi <jonasbgi@stud.ntnu.no>
2017-10-03 08:39:24 +0200

    copied concert/ from branch db

---
+++ concert/models.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.conf import settings
+#from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+class Manager(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+    def __str__(self):
+        return self.user.first_name + ' ' + self.user.last_name
+
+class Band(models.Model):
+    name = models.CharField(max_length=200)
+    manager = models.ForeignKey(Manager, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Stage(models.Model):
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200, null=True)
+    notes = models.CharField(max_length=2048, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Concert(models.Model):
+    name = models.CharField(max_length=200)
+    band = models.ForeignKey(Band, on_delete=models.SET_NULL, null=True)
+    stage = models.ForeignKey(Stage, on_delete=models.SET_NULL, null=True)
+    techs = models.ManyToManyField(settings.AUTH_USER_MODEL, through='Employment')
+    time = models.DateTimeField('Tidspunkt')
+
+    def __str__(self):
+        return self.name
+
+class Employment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    concert = models.ForeignKey(Concert, on_delete=models.CASCADE)
+    task = models.CharField(max_length=200)
+    notes = models.CharField(max_length=2048)
