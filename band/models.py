b9b2c3fad8412ae833859cb79f4aaece9236ced6
Erik Dymbe <erik.dymbe@gmail.com>
2017-10-03 11:30:19 +0200

    fix many2many

--- band/models.py
+++ band/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.conf import settings
 
+
 # Create your models here.
 class Band(models.Model):
     name = models.CharField(max_length=200)
-    genre = models.ManyToManyField(Genre)
     needs = models.CharField(max_length=200)
     manager = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
 
 
 class Genre(models.Model):
     name = models.CharField(max_length=200)
+    bands = models.ManyToManyField(Band, related_name="genre")
