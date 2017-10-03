dd08201315eee4b17602dcb0934d8c54a1d094e3
Erik Dymbe <erik.dymbe@gmail.com>
2017-10-03 11:18:20 +0200

    make model for Band, make model for Genre

--- band/models.py
+++ band/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Band(models.Model):
+    name = models.CharField(max_length=200)
+    genre = models.ManyToManyField(Genre)
+    needs = models.CharField(max_length=200)
+    manager = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
