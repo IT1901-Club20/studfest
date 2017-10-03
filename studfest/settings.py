1b605f4fee608db51955a5601cddc4f4f0ca453b
Erik Dymbe <erik.dymbe@gmail.com>
2017-10-03 11:34:08 +0200

    allow all hosts

--- studfest/settings.py
+++ studfest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'veldighemmeleg!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['studentfestival.club']
+ALLOWED_HOSTS = ['studentfestival.club', 'localhost', '*']
 
 
 # Application definition
