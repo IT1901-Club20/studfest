a30ffd3952f4bba7032227b7a5b1a1d569fee545
jonasbgi <jonasbgi@stud.ntnu.no>
2017-10-05 08:40:19 +0200

    Made template for concert

--- concert/views.py
+++ concert/views.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.template import loader
+from .models import Concert
+
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Ja, det er minst ein konsert på denne festivalen!")
+    concert = get_object_or_404(Concert, )
+    template = loader.get_template('concert/index.html')
+    context = {
+        'concert': concert
+    }
+    print(context)
+    return HttpResponse(template.render(context, request))
