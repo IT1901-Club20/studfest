bc07698c63eae7eb20c320f34a79f974c18811c7
Oegly <oivind@dirtcrate.com>
2017-10-06 16:58:18 +0200

    Endra ALLOWED_HOSTS for å samkøyre med Master.

--- studfest/settings.py
+++ studfest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'veldighemmeleg!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['studentfestival.club', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['studentfestival.club', 'localhost', '*']
 
 
 # Application definition
