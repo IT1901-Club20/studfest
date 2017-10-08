1661a0c5838209b5fe54f3d7b729fe177002ab72
Erik Dymbe <erik.dymbe@gmail.com>
2017-10-08 18:56:39 +0200

    add stuff

--- band/views.py
+++ band/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Band, Genre
 
 
-def index(response):
-    return HttpResponse("heisnan")
+def index(request):
+    template = loader.get_template('band/index.html')
+    bands = Band.objects.all()
+    context = {
+        'bands': bands,
+    }
+    return HttpResponse(template.render(context, request))
