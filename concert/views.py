3d1929fa1e8325557a132f8f3732aa7f296eb14b
Oegly <oivind@dirtcrate.com>
2017-10-09 22:05:12 +0200

    Implementering av svært enkel, men fungerande løysing for brukerhistorie #1 (Som arrangør av en konsert skal jeg kunne se en liste med hvem som hjelper til med rigging og tekniske oppgaver under konserten).

--- concert/views.py
+++ concert/views.py
@@ -6,13 +6,53 @@ from django.template import loader
 from .models import Concert
 
 
+# TODO: Veldig midlertidig, fiks snart!!!
+
+ORGANISER_GROUP_ID = 1
+
 # Create your views here.
 
+def group_access(user, *groups):
+    for g in user.groups.all():
+        if g.id in groups:
+            return True
+
+    return False
+
 def index(request):
-    concert = get_object_or_404(Concert, )
+    concert = get_object_or_404(Concert, pk=1)
     template = loader.get_template('concert/index.html')
     context = {
         'concert': concert
     }
     print(context)
+    return HttpResponse(template.render(context, request))
+
+def techs(request):
+    user = request.user
+    if not group_access(user, ORGANISER_GROUP_ID) and not user.is_superuser:
+            return HttpResponse(False)
+
+    if user.is_superuser:
+        # TODO: Sjå på kvifor dette ikkje funkar
+        concerts = Concert.objects.all()
+    else:
+        concerts = Concert.objects.filter(organiser=user.id)
+
+    employments = []
+    for concert in concerts:
+        for tech in concert.techs.all():
+            employments.append({'concert': concert.name[:32], 'tech': tech})
+
+    template = loader.get_template('concert/techs.html')
+
+    context = {'employments': employments}
+
+    ret = ''
+    for concert in concerts:
+        ret += '<p>' + concert.name + '</p>'
+    groups = request.user.groups.all()
+
+
+    #template.render(context, request)
     return HttpResponse(template.render(context, request))
