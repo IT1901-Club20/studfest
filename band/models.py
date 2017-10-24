2a4742b43dbb522187ec202c90a3d1a99a64e3b4
johanwei <you@example.com>
2017-10-24 10:59:56 +0200

    Lagt til nokkelinformasjon om band

--- band/models.py
+++ band/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
-
+#from concert.models import Concert
+#Concert = apps.get_model(app_label='concert', model_name='Concert')
 
 class Genre(models.Model):
     name = models.CharField(max_length=200)
@@ -13,6 +14,9 @@ class Band(models.Model):
     name = models.CharField(max_length=200)
     manager = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
     genres = models.ManyToManyField(Genre, related_name="bands")
+    streaming_popularity = models.IntegerField(default=0)
+    albums_sold = models.IntegerField(default=0)
+    #earlier_concerts = models.ForeignKey('concert.Concert')
 
     def __str__(self):
         genres = self.genres.all()
