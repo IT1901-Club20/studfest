8f3c82eaf5872609fdfe8e8e531afc614a623afc
Sagatun <eriksaga@stud.ntnu.no>
2017-10-31 12:57:47 +0100

    Merge branch 'master' of https://github.com/IT1901-Club20/studfest into EriksPlayground
    # Conflicts:
    #	concert/views.py
    #	db.sqlite3

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from booking.models import Offer
+from band.models import Band
 
 # Create your views here.
+
+def index(request):
+    a=Offer(band=Band.objects.get(pk=2), time='2017-10-12 21:00:00')
+    return HttpResponse(a.check_collision())
