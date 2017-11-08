f0258539276c70d1c91f2bfde663f10354b24253
Oegly <oivind@dirtcrate.com>
2017-11-08 20:27:22 +0100

    Added view for approving offers.

--- booking/urls.py
+++ booking/urls.py
@@ -9,5 +9,11 @@ urlpatterns = [
     url(r'^offer/send/(?P<id>[0-9]+)$', views.SendOffer.as_view(),
         name='send'),
     url(r'^offer/send/$', views.SendOffer.as_view(), name='send'),
+    url(r'^offer/(?P<pk>[0-9]+)/head-booker-approve/$',
+        views.ApproveOfferHeadBooker.as_view(),
+        name='head-booker-approve'),
+    url(r'^offer/(?P<pk>[0-9]+)/manager-approve/$',
+        views.ApproveOfferManager.as_view(),
+        name='manager-approve'),
     url(r'^offers$', views.OfferList.as_view(), name='index'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
