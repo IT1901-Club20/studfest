c2386a4a614f7e8ae6fb1eaedc2673d3a1434e5e
Oegly <oivind@dirtcrate.com>
2017-11-09 09:35:19 +0100

    Added unit tests for booking app.

--- booking/tests.py
+++ booking/tests.py
@@ -1,3 +1,88 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from datetime import datetime, timedelta
+from django.utils.timezone import make_aware
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
+
+from band.models import Band
+from booking.models import Offer
+from concert.models import Concert, Stage
 
 # Create your tests here.
+
+class OfferSendTest(TestCase):
+    def setUp(self):
+        for i in range(1,4):
+            User(username=str(i)).save()
+
+        band = Band()
+        stage = Stage()
+
+        band.save()
+        stage.save()
+
+        concert_date = make_aware(datetime(
+            year=2017,
+            month=11,
+            day=17,
+            hour=20
+        ))
+
+        Concert(
+            name="Veldig fredag",
+            organiser=User.objects.get(pk=1),
+            band=band,
+            stage=stage,
+            preparation_start=concert_date - timedelta(minutes=30),
+            time=concert_date,
+            time_end=concert_date + timedelta(hours=1),
+            takedown_end=concert_date + timedelta(hours=1, minutes=30),
+            needs="Ver flink!"
+        ).save()
+
+        Offer(
+            name="Veldig Laurdag",
+            band=band,
+            stage=stage,
+            time=concert_date,
+            booker=User.objects.get(pk=2),
+            monetary_offer=10
+        ).save()
+
+        Offer(
+            name="Veldig Laurdag",
+            band=band,
+            stage=stage,
+            time=concert_date + timedelta(days=1),
+            booker=User.objects.get(pk=2),
+            monetary_offer=10
+        ).save()
+
+
+    def test_invalid_offer(self):
+        o = Offer.objects.get(pk=1)
+        self.assertNotEqual(o.check_collision(), [[],[]])
+
+    def test_valid_offer(self):
+        o = Offer.objects.get(pk=2)
+        print(o.time)
+        self.assertEqual(o.check_collision(), [[],[]])
+
+    def test_create_concert(self):
+        o = Offer.objects.get(pk=2)
+
+        Concert(
+            name=o.name,
+            organiser=None, #User.objects.get(pk=1),
+            band=o.band,
+            stage=o.stage,
+            preparation_start=o.time - timedelta(minutes=30),
+            time=o.time,
+            time_end=o.time + timedelta(hours=1),
+            takedown_end=o.time + timedelta(hours=1, minutes=30),
+        ).save()
+
+        self.assertEqual(bool(Concert.objects.get(pk=2)), True)
